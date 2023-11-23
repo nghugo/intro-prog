2c1c9d3d52ca87e28fd26e65f806f3a52e1eefaa
louis-theo <147329277+louis-theo@users.noreply.github.com>
2023-11-23 15:59:06 +0000

    Prompt for create plan created
    Plans.py imported, as needs to be used to save plan data to plans.json inputted by user.
    
    Prompts user for plan details, and adds them to the plans.json file by using self.plans.add_plan() on line 213.
    
    Includes some basic data validation.

--- interface_admin_options.py
+++ interface_admin_options.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-from interface_helper import input_until_valid
-
+from interface_helper import input_until_valid, is_valid_date
+from plans import Plans
 
 class InterfaceAdminOptions:
     def __init__(self, users, current_user):
         self.users = users
         self.current_user = current_user
+        # initialise self.plans
+        self.plans = Plans()
 
     def execute_option(self, user_option):
 
@@ -23,7 +25,7 @@ class InterfaceAdminOptions:
         if user_option == "7":
             self.list_users()
         if user_option == "8":
-            pass  # TODO: implement
+            self.prompt_create_plan()
         if user_option == "9":
             pass  # TODO: implement
         if user_option == "10":
@@ -176,3 +178,43 @@ class InterfaceAdminOptions:
         users_df = pd.DataFrame.from_dict(self.users.users).transpose()
         print(users_df)
         print("--- End of users list ---")
+
+    def prompt_create_plan(self):
+        plan_name = input_until_valid(
+            input_message = "\nEnter plan name. This should be the name of the emergency occuring (E.g. Ukraine War):",
+            is_valid = lambda plan_name: plan_name != "",
+            validation_message = "plan name cannot be empty. Please enter an plan name."
+        )
+        description = input_until_valid(
+            input_message = "\nEnter plan description:",
+            is_valid = lambda description: description != "",
+            validation_message = "Plan description cannot be empty. Please enter a plan description."
+        )
+        location = input_until_valid(
+            input_message = "\nEnter location of the emergency:",
+            is_valid = lambda location: location != "",
+            validation_message = "Location cannot be empty. Please enter the location of the emergency."
+        )
+        start_date = input_until_valid(
+            input_message = "\nEnter the plan start date in the format dd/mm/yyyy:",
+            is_valid = lambda start_date: is_valid_date(start_date),
+            validation_message = "Invalid date format. Please re-enter the date in the format dd/mm/yyyy."
+        )
+        confirm = input_until_valid(
+            input_message = f"""Please confirm details of the new plan (y/n):\n
+            Plan name: {plan_name}\n
+            Plan Description: {description}\n
+            Plan Location: {location}\n
+            Plan Start Date: {start_date}""",
+            is_valid=lambda user_input: user_input == "y" or user_input == "n",
+            validation_message="Unrecognized input. Please confirm details of the new user (y/n):\n[y] Yes\n[n] No (abort)"
+            )
+        if confirm == "y":
+            success = self.plans.add_plan(
+                plan_name=plan_name, description=description, location=location, start_date=start_date)
+            if success:
+                print(f"Plan for {plan_name} successfully added.")
+            else:
+                print(f"Failed to add plan for {plan_name}")
+        else:
+            print(f"Aborted plan creation.")
