3c148bad51e862f3a2d3a3be01897c550c3da5c2
nghugo <nghugoryan@gmail.com>
2023-12-01 11:29:47 +0000

    write seed for dummy data

--- dummy.py
+++ dummy.py
@@ -45,8 +45,73 @@ dummy_users = {
     },  # for quick testing
 }
 
-def overwrite_users_json(dummyUsers): 
-    with open("users.json", "w") as json_file:
-        json.dump(dummyUsers, json_file)
+dummy_camps = {
+  "camp1": {
+    "location": "China",
+    "capacity": 20,
+    "humanitarian_plan_in": "planA",
+    "volunteer_in_charge": ["volunteer1", "v"]
+  },
+  "camp2": {
+    "location": "Ukraine",
+    "capacity": 224,
+    "humanitarian_plan_in": "planB",
+    "volunteer_in_charge": ["volunteer1", "volunteer2", "v"]
+  }
+}
 
-# overwrite_users_json(dummy_users)
+dummy_plans = {
+  "Ukraine war": {
+    "plan_name": "Ukraine war",
+    "description": "description",
+    "location": "kyiv",
+    "start_date": "24/12/2022"
+  },
+  "planA": {
+    "plan_name": "planA",
+    "description": "description for planA",
+    "location": "ucl",
+    "start_date": "25/09/2023"
+  },
+  "planB": {
+    "plan_name": "planB",
+    "description": "description for planA",
+    "location": "london",
+    "start_date": "05/01/2023"
+  }
+}
+
+dummy_refugees = {
+  "refugee1": {
+    "name": "John Doe",
+    "number_of_members": 1,
+    "camp_identification": "camp1",
+    "medical_condition": "food starved"
+  },
+  "refugee2": {
+    "name": "Jane Smith",
+    "number_of_members": 3,
+    "camp_identification": "camp1",
+    "medical_condition": "healthy"
+  },
+  "refugee3": {
+    "name": "Saul Goodman",
+    "number_of_members": 3,
+    "camp_identification": "camp2",
+    "medical_condition": "broken bones"
+  },
+  "f2db20d5525c49c2adaca68b15795f36": {
+    "name": "Hugo",
+    "number_of_members": 2,
+    "camp_identification": "camp1",
+    "medical_condition": "hungry"
+  }
+}
+
+
+
+def overwrite_json(object, file): 
+    with open(file, "w") as json_file:
+        json.dump(object, json_file)
+
+# overwrite_json(dummy_users)
