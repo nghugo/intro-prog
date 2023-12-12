31a2dfb0b167aedbf3c3f7faa0c0f1789a188e91
nghugo <nghugoryan@gmail.com>
2023-12-12 18:41:17 +0000

    bugfix remove duplicate plan input

--- interface_camp.py
+++ interface_camp.py
@@ -101,17 +101,6 @@ class InterfaceCamp:
 			validation_message = "Camp max_capacity must be a positive integer. Please re-enter."
 		)
 
-		plan_keys = Plans.load_all_plans().keys()
-		print(f"\nExisting plan(s): {','.join(plan_keys) if plan_keys else 'None found'}")
-		humanitarian_plan_in = input_until_valid(
-			input_message= "Please enter the humanitarian plan this camp belongs to. Choose from the above list, or leave empty to abort.",
-			is_valid = lambda user_input: user_input == "" or user_input in plan_keys,
-			validation_message = "Invalid value. Please enter the name of the humanitarian plan from the above list, or leave empty to abort."
-		)
-		if humanitarian_plan_in == "":
-			print("Camp creation aborted.")
-			return
-
 		confirm = input_until_valid(
 			input_message = f"Please confirm details of the new camp (y/n):\
 				\n->Camp ID: {camp_id}\
